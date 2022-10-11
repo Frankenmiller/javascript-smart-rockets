715ff84b58970b9ce97c50dd250250e32ed0b331
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-10-11 21:40:49 +0200

    Obstacles added

--- js/main.js
+++ js/main.js
@@ -1,28 +1,40 @@
 var population;
 var rockets;
-var lifespan = 200;
+var lifespan = 400;
 var life_P;
 var count = 0;
 var target;
 var maxforce = 0.2;
 var generation = 0;
 
+var rx_one = 175;
+var ry_one = 375;
+var rx_two = 175;
+var ry_two = 75;
+var rx_three = 175;
+var ry_three = 375;
+var rx_four = 175;
+var ry_four = 375;
+var rw = 175;
+var rh = 10;
+
+
 function setup()  {
-    document.getElementById('#my_canvas');
     createCanvas(350, 500);
     rocket = new Rocket();
     population = new Population();
     life_P = createP();
-    target = createVector(width/2, 50);
+    target = createVector(width -45, 40);
     fill(0, 0, 0);
     rect(0, 0, 10, 10);
 }
 
 function draw() {
-  background(0);
-  population.run();
-  life_P.html(count);
-  count++;
+    background(0);
+    population.run();
+    //life_P.html(count);
+    // strokeText(count, 10, 10);
+    count++;
   
   if  (count == lifespan) {
         population.evaluate();
@@ -32,6 +44,11 @@ function draw() {
         generation += 1;
     }
     fill(255, 255, 255);
+    rect(rx_two, ry_two, rw, rh)
+    // rect(0, 150, rw, rh)
+    // rect(175, 225, rw, rh)
+    //rect(rx, ry, rw, rh)
+    rect(rx_one, ry_one, rw, rh)
     ellipse(target.x, target.y, 16, 16);
     fill(255, 0, 0);
     ellipse(target.x, target.y, 12, 12);
@@ -39,7 +56,7 @@ function draw() {
     ellipse(target.x, target.y, 8, 8);
     fill(255, 0, 0);
     ellipse(target.x, target.y, 4, 4);
-    life_P.html(generation);
+    // life_P.html(generation);
 }
 
 function Population() {   
@@ -138,10 +155,11 @@ function DNA(genes) {
 
 
 function Rocket(dna) {    
-  this.pos = createVector(width/2, height);
+  this.pos = createVector(width / 2, height -25);
   this.vel =  createVector(0);
   this.acc = createVector();
   this.struck_target = false;
+  this.crashed = false;
   if (dna) {
     this.dna = dna;
   } else {
@@ -159,16 +177,33 @@ function Rocket(dna) {
     if (this.struck_target) {
         this.fitness *= 10;
     }
+    if (this.crashed) {
+        this.fitness = 1;
+    }
   }
   
   this.update = function() {
     var d = dist(this.pos.x, this.pos.y, target.x, target.y);
     if (d < 10) {
         this.struck_target = true;
+        this.pos = target.copy()
     }
+
+    if (this.pos.x > rx_one && this.pos.x < rx_one + rw && this.pos.y > ry_one && this.pos.y < ry_one + rh) {
+        this.crashed = true;
+    }
+    if (this.pos.x > rx_two && this.pos.x < rx_two + rw && this.pos.y > ry_two && this.pos.y < ry_two + rh) {
+        this.crashed = true;
+    }
+    if (this.pos.x > width || this.pos.x < 0) {
+        this.crashed = true;
+    }    
+    if (this.pos.y > height || this.pos.y < 0) {
+        this.crashed = true;
+    }    
     
     this.applyForce(this.dna.genes[count]);
-    if (!this.struck_target) {
+    if (!this.struck_target && !this.crashed) {
         this.vel.add(this.acc);
         this.pos.add(this.vel);
         this.acc.mult(0);
